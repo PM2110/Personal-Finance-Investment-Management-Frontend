fe6ca9825e7cf3e04481b9af073a42bce2ac759a
PM2110 <pmpatelmanan21@gmail.com>
2025-02-12 17:55:47 +0530

    Updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SignUpPage from "./pages/SignUpPage";
 import SignInPage from "./pages/SignInPage";
+import AddAccountPage from "./pages/AddAccountPage";
+import SendMoneyPage from "./pages/SendMoneyPage";
 
 const App = () => {
     return (
@@ -8,6 +10,8 @@ const App = () => {
           <Routes>
             <Route path="/" element={<SignUpPage />} />
             <Route path="/login" element={<SignInPage />} />
+            <Route path="/addAccount" element={<AddAccountPage />} />
+            <Route path="/sendMoney" element={<SendMoneyPage />} />
           </Routes>
       </BrowserRouter>
     );

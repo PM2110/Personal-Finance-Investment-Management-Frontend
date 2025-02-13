cf7d222972605e25673f0e8b7e9b58277d09edbd
PM2110 <pmpatelmanan21@gmail.com>
2025-02-13 18:09:46 +0530

    Create UI for Home Page (Page)

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import SideBarHomeComponent from "../components/home/SideBarHomeComponent";
+import HeaderHomeComponent from "../components/home/HeaderHomeComponent";
+import OtherIntegrationsComponent from "../components/other/OtherIntegrationsComponent";
+import MainRecipientsComponent from "../components/main/MainRecipientsComponent";
+import MainBalanceComponent from "../components/main/MainBalanceComponent";
+
+const HomePage = () => {
+    const [selected, setSelected] = useState(2);
+
+    const getForm = () => {
+        switch (selected){
+            case 1:
+                return ""
+            case 2:
+                return <MainBalanceComponent />
+            case 3:
+                return ""
+            case 4:
+                return ""
+            case 5:
+                return <MainRecipientsComponent />
+            case 6:
+                return <OtherIntegrationsComponent />
+        }
+    }
+
+    return (
+        <div className="flex items-center justify-between h-screen w-screen">
+            <SideBarHomeComponent selected={selected} setSelected={setSelected} />
+            <div className="flex flex-col items-center justify-start gap-8 w-full h-full px-8 py-10">
+                <HeaderHomeComponent selected={selected} />
+                {getForm()}
+            </div>
+        </div>
+    );
+}
+
+export default HomePage;

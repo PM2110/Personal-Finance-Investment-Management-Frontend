2914dac49ff5a4b75f9b6db8afab98f8ce7dbe00
PM2110 <pmpatelmanan21@gmail.com>
2025-02-13 18:04:48 +0530

    Few configurations & installations

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,15 @@ import SignUpPage from "./pages/SignUpPage";
 import SignInPage from "./pages/SignInPage";
 import AddAccountPage from "./pages/AddAccountPage";
 import SendMoneyPage from "./pages/SendMoneyPage";
+import HomePage from "./pages/HomePage";
 
 const App = () => {
     return (
         <BrowserRouter>
           <Routes>
-            <Route path="/" element={<SignUpPage />} />
+            <Route path="/signup" element={<SignUpPage />} />
             <Route path="/login" element={<SignInPage />} />
+            <Route path="/" element={<HomePage />} />
             <Route path="/addAccount" element={<AddAccountPage />} />
             <Route path="/sendMoney" element={<SendMoneyPage />} />
           </Routes>

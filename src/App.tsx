3b1615c924b980b67b233a5176b2641a47a12ff4
PM2110 <pmpatelmanan21@gmail.com>
2025-02-18 17:44:51 +0530

    Changed Fonts

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,16 @@ import SignInPage from "./pages/SignInPage";
 import AddAccountPage from "./pages/AddAccountPage";
 import SendMoneyPage from "./pages/SendMoneyPage";
 import HomePage from "./pages/HomePage";
+import EmailVerificationComponent from "./components/signup/EmailVerificationComponent";
+import EmailVerifiedComponent from "./components/signup/EmailVerifiedComponent";
 
 const App = () => {
     return (
         <BrowserRouter>
           <Routes>
             <Route path="/signup" element={<SignUpPage />} />
+            <Route path="/verifyEmail" element={<EmailVerificationComponent />} />
+            <Route path="/emailVerified" element={<EmailVerifiedComponent />} />
             <Route path="/login" element={<SignInPage />} />
             <Route path="/" element={<HomePage />} />
             <Route path="/addAccount" element={<AddAccountPage />} />
---
+++ tailwind.config.ts
@@ -0,0 +1,15 @@
+import type { Config } from 'tailwindcss'
+
+export default {
+    content: [
+      "./src/**/*.{js,jsx,ts,tsx}",
+    ],
+    theme: {
+      extend: {
+        fontFamily: {
+          sans: ['Poppins', 'sans-serif'],
+        },
+      },
+    },
+    plugins: [],
+} satisfies Config
